96a02ba1afd378a1e25f9e44cac159338c875d3e
iamtonmoy0 <tonmoy2code@gmail.com>
2023-03-02 21:54:42 -0500

    struct added

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use serde:{Deserialize,Serialize};
+
+
+#[derive(Serialize,Deserialize)]
+struct Paragraph{
+    name:"String"
+}
+
+#[derive(Serialize,Deserialize)]
+struct Article{
+    article:String,
+    author:String,
+    paragraph:Vec<Paragraph>
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    
 }
